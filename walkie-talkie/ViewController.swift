957b67ac027c681f0c39b67eed90b6e7871d26b0
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2022-09-21 13:08:51 -0400

    Initial Commit

---
+++ walkie-talkie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  walkie-talkie
+//
+//  Created by Parth Antala on 2022-09-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
